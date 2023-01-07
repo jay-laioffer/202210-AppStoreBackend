48519e85b6b358821324e300518a95372352bb15
jay-laioffer <114357091+jay-laioffer@users.noreply.github.com>
2023-01-07 11:14:33 -0800

    Add files via upload

--- handler/app.go
+++ handler/app.go
@@ -16,20 +16,22 @@ import (
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
-
-	fmt.Println("Received one upload request")
-
 	// Parse from body of request to get a json object.
 	fmt.Println("Received one upload request")
 
+	user := r.Context().Value("user")
+	claims := user.(*jwt.Token).Claims
+	username := claims.(jwt.MapClaims)["username"]
+
 	app := model.App{
 		Id:          uuid.New(),
-		User:        r.FormValue("user"),
+		User:        username.(string),
 		Title:       r.FormValue("title"),
 		Description: r.FormValue("description"),
 	}
 
 	price, err := strconv.ParseFloat(r.FormValue("price"), 64)
+	fmt.Printf("%v,%T", price, price)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -51,45 +53,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "App is saved successfully: %s\n", app.Description)
 }
-
-// func uploadHandler(w http.ResponseWriter, r *http.Request) {
-// 	// Parse from body of request to get a json object.
-// 	fmt.Println("Received one upload request")
-
-// 	user := r.Context().Value("user")
-// 	claims := user.(*jwt.Token).Claims
-// 	username := claims.(jwt.MapClaims)["username"]
-
-// 	app := model.App{
-// 		Id:          uuid.New(),
-// 		User:        username.(string),
-// 		Title:       r.FormValue("title"),
-// 		Description: r.FormValue("description"),
-// 	}
-
-// 	price, err := strconv.ParseFloat(r.FormValue("price"), 64)
-// 	fmt.Printf("%v,%T", price, price)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	app.Price = int(price * 100.0)
-
-// 	file, _, err := r.FormFile("media_file")
-// 	if err != nil {
-// 		http.Error(w, "Media file is not available", http.StatusBadRequest)
-// 		fmt.Printf("Media file is not available %v\n", err)
-// 		return
-// 	}
-
-// 	err = service.SaveApp(&app, file)
-// 	if err != nil {
-// 		http.Error(w, "Failed to save app to backend", http.StatusInternalServerError)
-// 		fmt.Printf("Failed to save app to backend %v\n", err)
-// 		return
-// 	}
-
-// 	fmt.Fprintf(w, "App is saved successfully: %s\n", app.Description)
-// }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one search request")
--- handler/user.go
+++ handler/user.go
@@ -13,6 +13,39 @@ import (
 	jwt "github.com/form3tech-oss/jwt-go"
 )
 
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one signup request")
+	w.Header().Set("Content-Type", "text/plain")
+
+	decoder := json.NewDecoder(r.Body)
+	var user model.User
+	if err := decoder.Decode(&user); err != nil {
+		http.Error(w, "Cannot decode user data from client", http.StatusBadRequest)
+		fmt.Printf("Cannot decode user data from client %v\n", err)
+		return
+	}
+
+	if user.Username == "" || user.Password == "" || regexp.MustCompile(`^[a-z0-9]$`).MatchString(user.Username) {
+		http.Error(w, "Invalid username or password", http.StatusBadRequest)
+		fmt.Printf("Invalid username or password\n")
+		return
+	}
+
+	success, err := service.AddUser(&user)
+	if err != nil {
+		http.Error(w, "Failed to save user to Elasticsearch", http.StatusInternalServerError)
+		fmt.Printf("Failed to save user to Elasticsearch %v\n", err)
+		return
+	}
+
+	if !success {
+		http.Error(w, "User already exists", http.StatusBadRequest)
+		fmt.Println("User already exists")
+		return
+	}
+	fmt.Printf("User added successfully: %s.\n", user.Username)
+}
+
 func signinHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one signin request")
 	w.Header().Set("Content-Type", "text/plain")
@@ -52,37 +85,4 @@ func signinHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte(tokenString))
-}
-
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Received one signup request")
-	w.Header().Set("Content-Type", "text/plain")
-
-	decoder := json.NewDecoder(r.Body)
-	var user model.User
-	if err := decoder.Decode(&user); err != nil {
-		http.Error(w, "Cannot decode user data from client", http.StatusBadRequest)
-		fmt.Printf("Cannot decode user data from client %v\n", err)
-		return
-	}
-
-	if user.Username == "" || user.Password == "" || regexp.MustCompile(`^[a-z0-9]$`).MatchString(user.Username) {
-		http.Error(w, "Invalid username or password", http.StatusBadRequest)
-		fmt.Printf("Invalid username or password\n")
-		return
-	}
-
-	success, err := service.AddUser(&user)
-	if err != nil {
-		http.Error(w, "Failed to save user to Elasticsearch", http.StatusInternalServerError)
-		fmt.Printf("Failed to save user to Elasticsearch %v\n", err)
-		return
-	}
-
-	if !success {
-		http.Error(w, "User already exists", http.StatusBadRequest)
-		fmt.Println("User already exists")
-		return
-	}
-	fmt.Printf("User added successfully: %s.\n", user.Username)
 }
